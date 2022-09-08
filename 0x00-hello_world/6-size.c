ccc4c3540e48195092a9fdf6c85953d8db25e110
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-09-08 16:38:57 -0700

    Task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - prints the size of many var types
+* main - prints the string in the put function
 *
 *Return: 0
 */
 int main(void)
 {
-	char a;
-	int b;
+	char c;
+	int i;
 	long int c;
 	long long int d;
-	float e;
-
-	printf("Size of a char: %d byte(s)\n", sizeof(a));
-	printf("Size of an int: %d byte(s)\n", sizeof(b));
-	printf("Size of a long int: %d byte(S)\n", sizeof(c));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-	printf("Size of a float: %d byte(s)\n", sizeof(e));
-	return (0);
-}
