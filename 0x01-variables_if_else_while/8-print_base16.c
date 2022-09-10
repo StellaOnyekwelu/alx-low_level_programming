d1dc0d192d66a44fab1468e6d8477d854a85d7c7
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-09-10 03:08:52 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - print all the number of base i6 in lowercaase
+*
+* Return: 0
+*/
+int main(void)
+{
+int d;
+char ch;
+
+for (d = '0'; d <= '9'; d++)
+{
+	putchar(d);
+}
+for (ch = 'a'; ch <= 'f'; low++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
