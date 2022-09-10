aadc0eae4631516c34e4c9a250275785acf5d83d
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-09-10 02:55:47 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - print all single digit numbers of base 10
+*
+* Return: 0
+*/
+int main(void)
+{
+int d;
+
+for (d = '0'; d <= '9'; d++)
+{
+	putchar(d);
+}
+putchar('\n');
+return (0);
+}
