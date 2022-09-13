dbd1a0468e4dbd4357f64ba26be3245418abdcef
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-09-13 14:42:41 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,12 @@
 /**
 * _islower - checks whether a character is a lowercase or not
 * @c: integer representing ascii value of character
-* Return: 1 if it is lowercase or 0 otherwise
+* Return: 1 if it is a lowercase ot 0 otherwsie
 */
 
-int -islower(int c)
+int _islower(int c)
 {
-if (c >= 97 && c <= 123)
+if (c >=97 && c <= 123)
 {
 return (1);
 }
