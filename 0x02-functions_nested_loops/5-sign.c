1a9bb1e358370fcd8af00b8c3c4a3929f87b343b
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-09-14 05:18:20 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,41 @@
 #include "main.h"
 /**
-* print_sign - prints sign of a number
+* print_sign - prints the sign of a number
 * @n: positive, zero or negative integer
-* Return 1 if positive, 0 if negative, -1 if negative
+* Return: 1 if positive, 0 if negative, -1 if negative
 */
+int print_sign(int n)                                                                                                                   
 
-int print_sign(int n)
-{
-if (n > 0)
-{
-	_putchar('+');
-	return (1);
-}
-else if (n < 0)
-{
-	_putchar('-');
-	return (-1);
-}
-else
-{
-	_putchar('0');
-	return (0);
-}
-}
+{                                                                                                                                       
+
+if (n > 0)                                                                                                                              
+
+{                                                                                                                                       
+
+        _putchar('+');                                                                                                                  
+
+        return (1);                                                                                                                     
+
+}                                                                                                                                       
+
+else if (n < 0)                                                                                                                         
+
+{                                                                                                                                       
+
+        _putchar('-');                                                                                                                  
+
+        return (-1);                                                                                                                    
+
+}                                                                                                                                       
+
+else                                                                                                                                    
+
+{                                                                                                                                       
+
+        _putchar('0');                                                                                                                  
+
+        return (0);                                                                                                                     
+
+}                                                                                                                                       
+
+}   
