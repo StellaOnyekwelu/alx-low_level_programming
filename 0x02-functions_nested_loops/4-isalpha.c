0fd692b5bc93e0920b7824b8610937b88e471169
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-09-14 05:03:51 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* _isalpha - checks if character is an alphabet or not
-* @c: acii value
+* _isalpha - checks if a character is an alphabet or not
+* @c: ascii value
 * Return: 1 if lowercase or 0
 */
 
 int _isalpha(int c)
 {
-if ((c >= 97 && c <= 123) || (c >=65 && c <= 91))
+if ((c >= 97 && c <= 123) || (c >= 65 && c <= 91))
 {
 return (1);
 }
