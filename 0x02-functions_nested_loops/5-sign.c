3e1030110407bf442577606275311d20e6df0b52
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-09-14 05:26:18 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,41 +1,25 @@
 #include "main.h"
 /**
-* print_sign - prints the sign of a number
-* @n: positive, zero or negative integer
+* print_sign - prints sign of number
+* @n: positive, zero or negative
 * Return: 1 if positive, 0 if negative, -1 if negative
 */
-int print_sign(int n)                                                                                                                   
 
-{                                                                                                                                       
-
-if (n > 0)                                                                                                                              
-
-{                                                                                                                                       
-
-        _putchar('+');                                                                                                                  
-
-        return (1);                                                                                                                     
-
-}                                                                                                                                       
-
-else if (n < 0)                                                                                                                         
-
-{                                                                                                                                       
-
-        _putchar('-');                                                                                                                  
-
-        return (-1);                                                                                                                    
-
-}                                                                                                                                       
-
-else                                                                                                                                    
-
-{                                                                                                                                       
-
-        _putchar('0');                                                                                                                  
-
-        return (0);                                                                                                                     
-
-}                                                                                                                                       
-
-}   
+int print_sign(int n)
+{
+if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+else if (n < 0)
+{
+	_putchar('-);
+	return (-1);
+}
+else
+{
+	_putchar('0');
+	return (0);
+}
+}
