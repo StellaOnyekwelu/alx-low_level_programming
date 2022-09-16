3b62d06cdb643bbbd57b6736b9ce9030ebf6dde1
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-09-16 04:35:00 -0700

    draws straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_line - prints line using "_" character of n length
+ *@n: length of the line
  *
- * Return: Always 0.
+ * Return: void
  */
-int main(void)
+void print_line(int n)
 {
-int i;
-int j;
-for (j = 0; j < 10; j++)
-{
-for (i = 0; i <= 14; i++)                               {
-if (i > 9)
-	_putchar((i / 10) + '0');
-	_putchar((i % 10) + '0');
-}
-_putchar('\n');
-}
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar(95);
+			i++;
+		}
+	}
+	_putchar('\n');
 }
