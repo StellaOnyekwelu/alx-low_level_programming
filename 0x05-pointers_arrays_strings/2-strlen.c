4e8d660fda244ce142d29f82679752cd4e9b2cc2
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-09-19 12:57:06 -0700

    a function that returns the length of a string.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,46 +1,15 @@
+#include "main.h"
 /**
- * _atoi - Converts a string to an integer
- * @s: The string to be converted
- * Return: The first integer in the string
+ * _strlen - gets character string
+ *
+ * @s: character to getstring
+ * Return: returns length
  */
-int _atoi(char *s)
+int _strlen(char *s)
 {
-	short sign = 1;
-	double result = 0;
-	int len, digits_seen_count, i, pass, positive, old_sign;
+	int i;
 
-	len = i = digits_seen_count = pass = old_sign = 0;
-	while (pass <= 1)
-	{
-		positive = old_sign != 0 ? old_sign == *(s + len) : *(s + len) == '+';
-		if (*(s + len) == '\0')
-		{
-			pass++;
-			len = 0;
-			continue;
-		}
-		else if (*(s + len) >= '0' && *(s + len) <= '9')
-		{
-			if (pass == 0)
-				digits_seen_count++;
-			else
-				result += (*(s + len) - '0') * b10_pow(digits_seen_count - i++ - 1);
-		}
-		else
-		{
-			if (pass == 0 && digits_seen_count > 0)
-			{
-				pass++;
-				len -= digits_seen_count;
-				continue;
-			}
-			if ((*(s + len) == '+' || *(s + len) == '-') && digits_seen_count == 0)
-			{
-				sign = positive ? 1 : 0 - 1;
-				old_sign = positive ? '+' : '-';
-			}
-		}
-		len++;
-	}
-	return (result * sign);
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
 }
