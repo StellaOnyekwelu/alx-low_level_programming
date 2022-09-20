8a5d25f1828f823d8658c7b7613046b285c49f53
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-09-20 01:49:42 -0700

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,20 +1,75 @@
+
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a given string
+ * puts_half - prints the second half of a given string
  * @str: passed string pointer
  *
  * Return: void
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
-	int i = 0;
+	int len = 0;
 
-	while (*(str + i) != '\0')
+	while (*(str + len) != '\0')
+		len++;
+	if (len % 2 == 0)
+		len /= 2;
+	else
+		len = len / 2 + 1;
+	while (*(str + len) != '\0')
 	{
-		if (i % 2 == 0)
-			_putchar(*(str + i));
-	i++;
+		_putchar(*(str + len));
+		len++;
 	}
-_putchar('\n');
+	_putchar('\n');
+}
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a given string
+ * @str: passed string pointer
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+		len++;
+	if (len % 2 == 0)
+		len /= 2;
+	else
+		len = len / 2 + 1;
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
+}#include "main.h"
+
+/**
+ * puts_half - prints the second half of a given string
+ * @str: passed string pointer
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+		len++;
+	if (len % 2 == 0)
+		len /= 2;
+	else
+		len = len / 2 + 1;
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
 }
