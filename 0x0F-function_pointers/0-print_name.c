fe2391524fcbb1233df05c89ad1872b775d6077b
StellaOnyekwelu <onyekwelustella70@gmail.com>
2022-10-12 13:27:46 -0700

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,21 +1,14 @@
 #include "function_pointers.h"
+
 /**
-*array_iterator - executes a function on each element of an array
-*@array: array to be targeted
-*@size: size of array
-*@action: function to be executed
-*
-*/
-void array_iterator(int *array, size_t size, void (*action)(int))
+ * print_name - prints name through passed in function pointer
+ *
+ * @name: character string
+ * @f: function pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
 {
-
-if (array == NULL || action == NULL)
-return;
-
-while (size-- > 0)
-{
-action(*array);
-array++;
-}
-
+	if (f)
+		f(name);
 }
